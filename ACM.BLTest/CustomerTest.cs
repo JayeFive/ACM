043a731094a4969c90dfa29d39594ec20b7f996b
Johnny Olson <john.olson3@gmail.com>
2018-12-18 15:16:47 -0800

    Add static instance count to customer class

--- ACM.BLTest/CustomerTest.cs
+++ ACM.BLTest/CustomerTest.cs
@@ -55,5 +55,28 @@ namespace ACM.BLTest
             // Assert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void StaticTest()
+        {
+            // Arrange
+            var c1 = new Customer();
+            c1.FirstName = "Bilbo";
+            Customer.InstanceCount++;
+
+            var c2 = new Customer();
+            c2.FirstName = "Frodo";
+            Customer.InstanceCount++;
+
+
+            var c3 = new Customer();
+            c3.FirstName = "Rosie";
+            Customer.InstanceCount++;
+
+            // Act
+
+            // Assert   
+            Assert.AreEqual(3, Customer.InstanceCount);
+        }
     }
 }
