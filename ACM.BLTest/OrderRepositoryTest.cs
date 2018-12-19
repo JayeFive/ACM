e2ea112a7c9c54562768318ae9dfba6f715e57dc
Johnny Olson <john.olson3@gmail.com>
2018-12-18 17:00:54 -0800

    Add OrderRepository unit tests

---
+++ ACM.BLTest/OrderRepositoryTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ACM.BL;
+
+namespace ACM.BLTest
+{
+    [TestClass]
+    public class OrderRepositoryTest
+    {
+        [TestMethod]
+        public void RetrieveExisting()
+        {
+            // Arrange
+            var orderRepository = new OrderRepository();
+            var expected = new Order(10)
+            {
+                OrderDate = new DateTimeOffset(2018, 11, 30, 17, 0, 0, new TimeSpan(-8, 0, 0))
+            };
+
+            // Act
+            var actual = orderRepository.Retrieve(1);
+
+            // Assert
+            Assert.AreEqual(expected.OrderDate, actual.OrderDate);
+        }
+    }
+}
