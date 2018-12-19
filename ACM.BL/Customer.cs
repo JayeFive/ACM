9aa835b5d961e847eb413671c91b3ec053e82126
Johnny Olson <john.olson3@gmail.com>
2018-12-18 17:40:49 -0800

    Add address list so customers can have multiple addresses

--- ACM.BL/Customer.cs
+++ ACM.BL/Customer.cs
@@ -8,13 +8,14 @@ namespace ACM.BL
 {
     public class Customer
     {
-        public Customer()
+        public Customer() : this(0)
         {
         }
 
         public Customer(int customerId)
         {
             this.CustomerId = customerId;
+            AddressList = new List<Address>();
         }
 
         public static int InstanceCount { get; set; }
@@ -23,6 +24,8 @@ namespace ACM.BL
         public string LastName { get; set; }
         public string EmailAddress { get; set; }
         public int CustomerId { get; private set; }
+
+        public List<Address> AddressList { get; set; }
 
         public string FullName
         {
