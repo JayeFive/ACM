76b1f43bd53401ad7b0bbcaee94c90aedc9cbec6
Johnny Olson <john.olson3@gmail.com>
2018-12-18 16:07:31 -0800

    Add Order class

---
+++ ACM.BL/Order.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class Order
+    {
+        public Order()
+        {
+        }
+
+        public Order(int orderId)
+        {
+            this.OrderId = orderId;
+        }
+
+        public DateTimeOffset? OrderDate { get; set; }
+        public int OrderId { get; private set; }
+
+        /// <summary>
+        /// Retrieve one order
+        /// </summary>
+        /// <returns></returns>
+        public Order Retrieve(int orderId)
+        {
+            return new Order();
+        }
+
+        /// <summary>
+        /// Save the current order
+        /// </summary>
+        /// <returns></returns>
+        public bool Save()
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Validate the order data
+        /// </summary>
+        /// <returns></returns>
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (OrderDate == null) isValid = false;
+
+            return isValid;
+        }
+    }
+}
