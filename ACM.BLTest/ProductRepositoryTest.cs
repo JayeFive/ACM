ed1140e91171348ec6b47cbc7882a2e2af0ae2d7
Johnny Olson <john.olson3@gmail.com>
2018-12-18 17:06:06 -0800

    Add ProductRepository unit tests

---
+++ ACM.BLTest/ProductRepositoryTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ACM.BL;
+
+namespace ACM.BLTest
+{
+    [TestClass]
+    public class ProductRepositoryTest
+    {
+        [TestMethod]
+        public void RetrieveExisting()
+        {
+            // Arrange
+            var productRepository = new ProductRepository();
+            var expected = new Product(2)
+            {
+                ProductName = "Sunflowers",
+                ProductDescription = "Assorted sizes of sunflowers",
+                CurrentPrice = 15.96m
+            };
+
+            // Act
+            var actual = productRepository.Retrieve(2);
+
+            // Assert
+            Assert.AreEqual(expected.ProductName, actual.ProductName);
+            Assert.AreEqual(expected.ProductDescription, actual.ProductDescription);
+            Assert.AreEqual(expected.CurrentPrice, actual.CurrentPrice);
+        }
+    }
+}
